af4843fc653681465fda135ed00582ecf3825f4f
MarenSusanne <Marensut@getacademy.no>
2024-05-22 13:38:00 +0200

    Add project files.

---
+++ ParProgramering/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParProgramering
+{
+    public class Person
+    {
+        public string Name;
+        public string Age;
+        public string HairColor;
+        public string Hobby;
+        public string FavoriteMeal;
+        public Person(string name, string age, string hairColor, string hobby, string favoriteMeal)
+        {
+            Name = name;
+            Age = age;
+            HairColor = hairColor;
+            Hobby = hobby;
+            FavoriteMeal = favoriteMeal;
+            
+        }
+    }
+}
---
+++ ParProgramering/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ParProgramering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Bjarne = new Person("Bjarne", "42", "brown", "fishing", "beer");
+            Console.WriteLine($"Name: {Bjarne.Name}\nAge: {Bjarne.Age}\nHaircolor: {Bjarne.HairColor}\nHobby: {Bjarne.Hobby}\nFavorite meal: {Bjarne.FavoriteMeal}");
+            var Kurt = new Person("Kurt", "38", "black", "football", "hot dogs");
+            Console.WriteLine($"Name: {Kurt.Name}\nAge: {Kurt.Age}\nHaircolor: {Kurt.HairColor}\nHobby: {Kurt.Hobby}\nFavorite meal: {Kurt.FavoriteMeal}");
+
+            Console.WriteLine("Enter Name:");
+            var personName = Console.ReadLine();
+            Console.WriteLine("Enter Age:");
+            var personAge = Console.ReadLine();
+            Console.WriteLine("Enter Hair Color:");
+            var personHairColor = Console.ReadLine();
+            Console.WriteLine("Enter Hobby:");
+            var personHobby = Console.ReadLine();
+            Console.WriteLine("Enter Favorite Meal:");
+            var personFavoriteMeal = Console.ReadLine();
+            var newPerson = new Person($"{personName}", $"{personAge}", $"{personHairColor}", $"{personHobby}", $"{personFavoriteMeal}");
+            Console.WriteLine($"Name: {newPerson.Name}\nAge: {newPerson.Age}\nHaircolor: {newPerson.HairColor}\nHobby: {newPerson.Hobby}\nFavorite meal: {newPerson.FavoriteMeal}");
+        }
+    }
+}
